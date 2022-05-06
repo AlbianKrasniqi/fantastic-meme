06be7f7e6b60c072a93a063a18fcaf7fd15fb4db
blerimb1 <blerim_bojaxhi@hotmail.com>
2022-05-06 18:14:53 +0200

    Detyra e nente 1.1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-console.log("Coding 4");
-=======
-console.log("Coding 45");
->>>>>>> c94ae7363ca94cdb54f4dd95f1ff7dbee79d0ff5
+
+
+let uc = require("upper-case");
+
+console.log(uc.upperCase("test"));
---
+++ index1.js
@@ -0,0 +1,86 @@
+// // let a = 1;
+// // let b = "kosova";
+// // let c = true;
+// // const d = 1.000001;
+// // let e = [1,1,"kosova",2];
+// // if(a === d && e[1] === 1){
+// //     console.log(b);
+// // }
+// // else if(e[0] === "1"){
+// //     console.log(b +" republik");
+// // }
+// // else{
+// //     console.log("Java" + 'Script');
+// // } 
+// // for (let index = 0; index < array.length; index++) {
+// //     const element = array[index];
+    
+// // }
+
+// // for (const key in object) {
+// //     if (Object.hasOwnProperty.call(object, key)) {
+// //         const element = object[key];
+        
+// //     }
+// // }
+// let uc = require("upper-case");
+
+// let user =[
+//     {
+// name:"Artan",
+// surname:"Bojaxhi",
+// age: 18,
+// city:"Prizren",
+// postalCode:1000
+// },
+// {
+//     name:"Agim",
+//     surname:"Bojaxhi",
+//     age: 18,
+//     city:"Prizren",
+//     postalCode:1000
+// },
+// {
+//     name:"Arber",
+//     surname:"Bojaxhi",
+//     age: 18,
+//     city:"Prizren",
+//     postalCode:1000,
+//     lagjja: "Bazhdarhane"
+// },
+// {
+//         name:"Sejfi",
+//         surname:"Bojaxhi",
+//         age: 18,
+//         city:"Prizren",
+//         postalCode:1000,
+        
+// },
+// {
+//         name:"Kosova",
+//         surname:"Bojaxhi",
+//         age: 18,
+//         city:"Prizren",
+//         postalCode:1000
+// },
+// ]
+// for (const key of user) {
+//     key.name = uc.upperCase(key.name);
+// }
+
+let arr = [1,2,3,4,5];
+// arr.pop();// e hjek elementin e fundit
+// arr.push(); //  me shtu ni element nfund
+// arr.shift(); // e fshin tparin
+// arr.unshift(0);// e shton nfillim ni element
+// arr.splice();// Prej cilit element deri ncilin element me fshi
+// arr.slice(5);  //
+// arr.includes(num); // a esht qaj numri nqet array
+// arr.indexOf // indexi i elementit
+// arr.concat(num); me shtu ni element nfund
+// arr.forEach((element) =>{
+
+// }
+// )
+console.log(arr.reverse());
+// console.log(user);
